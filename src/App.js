abd298a4c0fab301650c3b165af125750e9131c1
Kukyeon <rnrdus2000@naver.com>
2025-10-13 12:52:45 +0900

    회원가입 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+
+import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './component/Navbar';
+import Home from './pages/Home';
+import Board from './pages/Board';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import BoardDetail from './pages/BoardDetail';
+import BoardWrite from './pages/BoardWrite';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/board' element={<Board />}></Route>
+        <Route path='/board/write' element={<BoardWrite />}></Route>
+        <Route path='/board/:id' element={<BoardDetail />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/component/Navbar.js
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <div className="logo" >홈페페페이지</div>
+            <div className="menu">
+                <Link to="/">Home</Link>
+                <Link to="/board">게시판</Link>
+                <Link to="/login">로그인</Link>
+                <Link to="/signup">회원가입</Link>
+                <button className="logout-btn">로그아웃</button>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
